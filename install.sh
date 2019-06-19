ab8c0cfbb5676a607ac29da0f7dd99f36fa51094
mikibouns <mikibouns@gmail.com>
2019-06-19 11:59:46 +0200

    added container name in docker-compose.yml

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-docker-compose build && docker-compose up -d && docker exec -it django_rest_project_docker_python_1 python manage.py fill_db
+docker-compose build && docker-compose up -d && docker exec -it python_project python manage.py fill_dbn_
